2cbb36cc4ea725acc35ed4dad9cc116158bcaac2
UrubuDPIX <okairuskitz192@gmail.com>
2026-06-10 01:59:08 -0300

    feat: Rewrite Forge mod to native NeoForge 1.21.1 implementation

---
+++ plugin-sync/forge-mod/src/main/java/com/urubudpix/playermanagersync/PlayerManagerSync.java
@@ -0,0 +1,44 @@
+package com.urubudpix.playermanagersync;
+
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.tick.ServerTickEvent;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+import net.neoforged.neoforge.server.ServerLifecycleHooks;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("playermanagersync")
+public class PlayerManagerSync {
+    private static final Logger LOGGER = LogManager.getLogger();
+    private int tickCount = 0;
+
+    public PlayerManagerSync() {
+        NeoForge.EVENT_BUS.register(this);
+        LOGGER.info("PlayerManagerSync (NeoForge 1.21.1) Initialized!");
+    }
+
+    @SubscribeEvent
+    public void onServerTick(ServerTickEvent.Post event) {
+        tickCount++;
+        // 5 seconds = 100 ticks
+        if (tickCount >= 100) {
+            tickCount = 0;
+            MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+            if (server != null) {
+                int count = 0;
+                for (ServerPlayer player : server.getPlayerList().getPlayers()) {
+                    // This forces a save to world/playerdata/UUID.dat
+                    server.getPlayerList().save(player);
+                    count++;
+                }
+                if (count > 0) {
+                    LOGGER.debug("Saved inventory data for " + count + " players.");
+                }
+            }
+        }
+    }
+}
