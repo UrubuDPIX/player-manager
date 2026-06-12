4297310fd060801fe926c4f1f6155ec3843e8815
UrubuDPIX <okairuskitz192@gmail.com>
2026-06-11 23:34:01 -0300

    fix: hardcode component download URLs to bypass github raw cache

--- install-vps.sh
+++ install-vps.sh
@@ -273,8 +273,8 @@ const panelDir = process.argv[2];
     const targetPath = path.join(panelDir, 'resources/scripts/components/dashboard/ServerRow.tsx');
     const layoutManagerPath = path.join(panelDir, 'resources/scripts/components/dashboard/LayoutManager.tsx');
     try {
-        require('child_process').execSync('curl -sSL "https://raw.githubusercontent.com/UrubuDPIX/player-manager/master/client/components/ServerRowArix.tsx?v=' + Date.now() + '" -o "' + targetPath + '"');
-        require('child_process').execSync('curl -sSL "https://raw.githubusercontent.com/UrubuDPIX/player-manager/master/client/components/LayoutManager.tsx?v=' + Date.now() + '" -o "' + layoutManagerPath + '"');
+        require('child_process').execSync('curl -sSL "https://raw.githubusercontent.com/UrubuDPIX/player-manager/97c7ba19225f8f7aeadb74b9c6c6fde1f1c30da4/client/components/ServerRowArix.tsx?v=' + Date.now() + '" -o "' + targetPath + '"');
+        require('child_process').execSync('curl -sSL "https://raw.githubusercontent.com/UrubuDPIX/player-manager/97c7ba19225f8f7aeadb74b9c6c6fde1f1c30da4/client/components/LayoutManager.tsx?v=' + Date.now() + '" -o "' + layoutManagerPath + '"');
         console.log('✓ Arix Premium ServerRow instalado com sucesso!');
     } catch (e) {
         console.error(' Erro ao baixar o Arix ServerRow:', e);
