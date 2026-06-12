05f9a368ae451f1f0b85f68f50c3e112480b5bb7
UrubuDPIX <okairuskitz192@gmail.com>
2026-06-12 00:55:54 -0300

    fix: revert serverrow path to dashboard and use confirmed commit hash

--- install-vps.sh
+++ install-vps.sh
@@ -270,11 +270,11 @@ const panelDir = process.argv[2];
 // 4. Download Arix Premium ServerRow & LayoutManager
 (function installArixServerRow() {
     console.log(' Baixando Arix Premium ServerRow & LayoutManager...');
-    const targetPath = path.join(panelDir, 'resources/scripts/components/account/ServerRow.tsx');
-    const layoutManagerPath = path.join(panelDir, 'resources/scripts/components/account/LayoutManager.tsx');
+    const targetPath = path.join(panelDir, 'resources/scripts/components/dashboard/ServerRow.tsx');
+    const layoutManagerPath = path.join(panelDir, 'resources/scripts/components/dashboard/LayoutManager.tsx');
     try {
-        require('child_process').execSync('curl -sSL "https://raw.githubusercontent.com/UrubuDPIX/player-manager/133d1ff35003feefa9dcbe371947eaff435a9171/client/components/ServerRowArix.tsx?v=' + Date.now() + '" -o "' + targetPath + '"');
-        require('child_process').execSync('curl -sSL "https://raw.githubusercontent.com/UrubuDPIX/player-manager/133d1ff35003feefa9dcbe371947eaff435a9171/client/components/LayoutManager.tsx?v=' + Date.now() + '" -o "' + layoutManagerPath + '"');
+        require('child_process').execSync('curl -sSL "https://raw.githubusercontent.com/UrubuDPIX/player-manager/8490db12085b221c2260bc6cca140899d29dbd5a/client/components/ServerRowArix.tsx" -o "' + targetPath + '"');
+        require('child_process').execSync('curl -sSL "https://raw.githubusercontent.com/UrubuDPIX/player-manager/8490db12085b221c2260bc6cca140899d29dbd5a/client/components/LayoutManager.tsx" -o "' + layoutManagerPath + '"');
         console.log('✓ Arix Premium ServerRow instalado com sucesso!');
     } catch (e) {
         console.error(' Erro ao baixar o Arix ServerRow:', e);
